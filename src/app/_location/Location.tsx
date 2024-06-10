a91577611cfdd242531a5ce549eeb6e9fafba930
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-06-10 16:58:11 +0100

    get location on load

--- src/app/_location/Location.tsx
+++ src/app/_location/Location.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import { getWeatherForecast } from '../_util/getWeatherForecast';
 import { WeatherData } from '../_models';
@@ -9,37 +9,34 @@ export default function Location() {
   const [weatherData, setWeatherData] = useState<WeatherData | null>();
   const [error, setError] = useState<string | null>(null);
 
-  const getLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const location = {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-          };
-          getWeatherForecast(location).then((data) => setWeatherData(data));
-          setError(null);
-        },
-        (error) => {
-          setError(error.message);
-        }
-      );
-    } else {
-      setError('Geolocation is not supported by this browser');
-    }
-  };
+  useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const location = {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            };
+            getWeatherForecast(location).then((data) => setWeatherData(data));
+            setError(null);
+          },
+          (error) => {
+            setError(error.message);
+          }
+        );
+      } else {
+        setError('Geolocation is not supported by this browser');
+      }
+    };
+    getLocation();
+  }, []);
 
   return (
     <div>
-      <button
-        className='bg-chalk text-black px-3 py-2 rounded hover:bg-gray-200 transition'
-        onClick={getLocation}
-      >
-        Get Location
-      </button>
       {error && <p>Error: {error}</p>}
 
-      <GooglePlacesAutocomplete apiKey='AIzaSyC7CRMGidfgMRFxUD5Q1cAN00XO17fdHi4' />
+      {/* <GooglePlacesAutocomplete apiKey='AIzaSyC7CRMGidfgMRFxUD5Q1cAN00XO17fdHi4' /> */}
       {weatherData && (
         <div className='text-chalk'>
           <p>You are in {weatherData.location}</p>
