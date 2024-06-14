9b3983259d62adebddb6580580692da7f2c496b0
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-06-14 17:43:34 +0100

    responsive styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,13 +24,13 @@ export default function RootLayout({
         <body className={`${openSans.className} h-screen`}>
           <main className='w-full text-center grid-cols-1 h-screen'>
             {children}
-            <div className='grid grid-cols-2'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 h-3/5'>
               <Column>{weather}</Column>
               <Column>
                 <p>Spotify goes here</p>
               </Column>
             </div>
-            <div className='h-32 justify-self-end'>Player goes here</div>
+            <div className='h-1/5 justify-self-end'>Player goes here</div>
           </main>
         </body>
       </WeatherContextProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className='h-32 flex items-center justify-center'>
+    <div className='h-1/5 flex items-center justify-center'>
       <h1 className='font-thin text-4xl text-chalk'>Music for the weather</h1>
     </div>
   );
