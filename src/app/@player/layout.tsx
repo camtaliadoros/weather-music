e4b0cf980d8325932b686b998b45291ca15dfc9a
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-06-28 17:45:14 +0100

    playlist player clash issue

--- src/app/@player/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SpotifyContextProvider } from '../_contexts/SpotifyAuthContextProvider';
-
-export default function PlayerLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SpotifyContextProvider>{children}</SpotifyContextProvider>;
-}
--- src/app/@playlist/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SpotifyContextProvider } from '../_contexts/SpotifyAuthContextProvider';
-
-export default function PlaylistLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SpotifyContextProvider>{children}</SpotifyContextProvider>;
-}
