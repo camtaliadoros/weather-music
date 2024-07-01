33bf828e0ae7edffc15805ab6f03e3227a6d2783
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-07-01 12:57:05 +0100

    button styling

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,5 +4,12 @@ type ButtonPropsType = {
 };
 
 export default function Button({ label, onClickAction }: ButtonPropsType) {
-  return <button onClick={onClickAction}>{label}</button>;
+  return (
+    <button
+      className='py-4 px-2 mt-4 bg-chalk rounded-sm transition hover:bg-opacity-80'
+      onClick={onClickAction}
+    >
+      {label}
+    </button>
+  );
 }
