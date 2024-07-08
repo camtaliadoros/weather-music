799ff198f6e093c6f0d8002f8e2e878636e8b7bd
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-07-08 12:42:33 +0100

    ignore casing

---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+import { PropagateLoader } from 'react-spinners';
+
+type ButtonPropsType = {
+  label: string;
+  onClickAction: () => void;
+  loading: boolean;
+  classes: string;
+};
+
+export const Button = ({
+  label,
+  onClickAction,
+  loading,
+  classes,
+}: ButtonPropsType) => {
+  return (
+    <button
+      className={` h-16 my-4 ring-1 ring-chalk rounded-full text-chalk transition hover:bg-chalk hover:bg-opacity-30  ${classes}`}
+      onClick={onClickAction}
+    >
+      <PropagateLoader
+        className='h-4'
+        color='#F8F9FA'
+        loading={loading}
+        size={16}
+      />
+      {!loading && label}
+    </button>
+  );
+};
