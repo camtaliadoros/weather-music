2922034c432b95e5b453167e99895da7d911373d
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2024-07-08 12:37:42 +0100

    uppercase change for components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from '@/components/button';
+import { Button } from '@/components/Button';
 import { useState } from 'react';
 
 const weatherView = {
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
-export { Button } from './button';
+export { Button } from './Button';
 export { Column } from './column';
 export { Loading } from './loading';
-export { Track } from './track';
+export { Track } from './Track';
 export { TrackArtists } from './trackArtists';
 export { TrackControlIcon } from './trackControlIcon';
 export { TrackName } from './trackName';
